272a636d056fff4f816d500f3a7575412dcb9498
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2024-06-10 09:09:51 +0700

    configuration and linter setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import PropTypes from 'prop-types';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Blog App',
   description: 'The best blog app!',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">{children}</div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
+
+RootLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
-import styles from "./homepage.module.css";
+import styles from './homepage.module.css';
+import Link from 'next/link';
+// import React from 'react';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">Hello</Link>
+    </div>
+  );
 }
