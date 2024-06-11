8f3ad1d28d0980b14f6845444d4e87156750df56
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2024-06-11 08:27:10 +0700

    navbar done

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './authLinks.module.css';
 import Link from 'next/link';
 
 const authLinks = () => {
+  const [open, setOpen] = useState(false);
+
   const status = 'authenticated';
-  return status === `notauthenticated` ? (
-    <Link href="/login">Login</Link>
-  ) : (
+
+  return (
     <>
-      <Link href="/write">Write</Link>
-      <span className={styles.link}>Logout</span>
+      {status === 'notauthenticated' && (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      )}
+      {status === 'authenticated' && (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          {status === 'notauthenticated' ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
     </>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,31 +9,22 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image
-          src="/images/instagram.png"
-          alt="instagram"
-          width={25}
-          height={25}
-        />
-        <Image
-          src="/images/facebook.png"
-          alt="facebook"
-          width={25}
-          height={25}
-        />
-        <Image
-          src="/images/youtube.png"
-          alt="youtube"
-          width={25}
-          height={25}
-        />
+        <Image src="/images/instagram.png" alt="instagram" width={25} height={25} />
+        <Image src="/images/facebook.png" alt="facebook" width={25} height={25} />
+        <Image src="/images/youtube.png" alt="youtube" width={25} height={25} />
       </div>
       <div className={styles.logo}>RS Siaga Utama</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/about">About</Link>
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/contact" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/about" className={styles.link}>
+          About
+        </Link>
         <AuthLinks />
       </div>
     </div>
