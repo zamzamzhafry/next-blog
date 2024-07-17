07c38f5f62f05136c8f8ab4ffa20d0721f156c3d
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2024-07-18 05:58:12 +0700

    tes update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
+  title: 'RS Blog App',
   description: 'The best blog app!',
 }
 
